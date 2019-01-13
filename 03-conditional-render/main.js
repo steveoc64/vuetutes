ffc65fc6abc457ff30f8167452c8e19cfa7e0e35
steveoc <steveoc64@gmail.com>
2019-01-13 15:37:06 +1000

    added 3

---
+++ 03-conditional-render/main.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Socks',
+        image: 'vuesocks.jpg',
+        url: 'http://theoldowlsscarf.com',
+        inventory: 100,
+        inStock: true,
+        onSale: false,
+    }
+})
