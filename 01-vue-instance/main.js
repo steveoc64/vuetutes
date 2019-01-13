075abdc6ce795c502e553b83a6ae2169cf1c1bf0
steveoc <steveoc64@gmail.com>
2019-01-13 15:26:41 +1000

    first and second blood

---
+++ 01-vue-instance/main.js
@@ -0,0 +1,9 @@
+var app = new Vue(
+    {
+        el: '#app',
+        data: {
+            product: 'Socks',
+            descr: 'a description of the thing above'
+        }
+    }
+)
---
+++ 02-attrib-binding/main.js
@@ -0,0 +1,8 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Socks',
+        image: 'vuesocks.jpg',
+        url: 'http://theoldowlsscarf.com',
+    }
+})
