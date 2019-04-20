2d2b898ee7b4f54722572a699e85a6a91e184356
SteveOC <steven.oconnor@neds.com>
2019-04-20 12:49:27 +1000

    lesson 7 - computed properies

--- 07-computed-props/main.js
+++ 07-computed-props/main.js
@@ -1,11 +1,10 @@
 var app = new Vue({
     el: '#app',
     data: {
+        brand: 'VueMastery',
         product: 'Socks',
-        image: 'vuesocks-green.jpg',
         url: 'http://theoldowlsscarf.com',
-        inventory: 100,
-        inStock: true,
+        variant: 0,
         onSale: false,
         details: [
             "green socks",
@@ -13,25 +12,41 @@ var app = new Vue({
             "and even more",
         ],
         variants: [
-            { id: 1, color: 'green', image: 'vuesocks-green.jpg'},
-            { id: 2, color: 'red', image: 'vuesocks-green.jpg'},
-            { id: 3, color: 'blue', image: 'vuesocks-blue.jpg'},
-            { id: 4, color: 'yellow', image: 'vuesocks-blue.jpg'},
+            {id: 1, color: 'green', image: 'vuesocks-green.jpg', inventory: 10},
+            {id: 2, color: 'red', image: 'vuesocks-green.jpg', inventory: 10},
+            {id: 3, color: 'blue', image: 'vuesocks-blue.jpg', inventory: 10},
+            {id: 4, color: 'yellow', image: 'vuesocks-blue.jpg', inventory: 10},
         ],
         cart: 0,
     },
-    methods: {
-        addToCart(){
-            this.cart += 1
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
         },
-        updateImage(url) {
-            this.image = url
+        image() {
+            return this.variants[this.variant].image
+        },
+        inStock() {
+            return this.variants[this.variant].inventory > 0
+        },
+        inventory() {
+            return this.variants[this.variant].inventory
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1;
+            this.variants[this.variant].inventory--;
+        },
+        updateImage(i) {
+            this.variant = i
         },
         takeFromCart() {
-            this.cart -= 1
+            this.cart -= 1;
             if (this.cart <= 0) {
                 this.cart = 0
             }
+            this.variants[this.variant].inventory++
         },
     },
-})
+});
